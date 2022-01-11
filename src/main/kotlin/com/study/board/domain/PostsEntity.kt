3be583828fe4628389d1935ea2577fc0897703cd
055055 <snb4301@gmail.com>
2022-01-12 00:04:48 +0900

    entity field setter 접근제한 추가

--- src/main/kotlin/com/study/board/domain/PostsEntity.kt
+++ src/main/kotlin/com/study/board/domain/PostsEntity.kt
@@ -8,10 +8,15 @@ class PostsEntity(author: String, password: String, title: String, content: Stri
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var seq: Long? = null
+        protected set
     var author: String = author
+        protected set
     var password: String = password
+        protected set
     var title: String = title
+        protected set
     var content: String = content
+        protected set
 
     fun update(author: String, password: String, title: String, content: String) {
        this.author = author
