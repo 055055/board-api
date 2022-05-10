8e60f6b68f8a0a9b197fe587a3fb870a6f7db777
055055 <snb4301@gmail.com>
2022-05-11 00:11:40 +0900

    Elasticsearch Config 추가

---
+++ src/main/kotlin/com/study/board/config/EsConfig.kt
@@ -0,0 +1,22 @@
+package com.study.board.config
+
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.client.ClientConfiguration
+import org.springframework.data.elasticsearch.client.RestClients
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
+import java.time.Duration
+
+@Configuration
+class EsConfig: AbstractElasticsearchConfiguration() {
+
+    override fun elasticsearchClient(): RestHighLevelClient {
+        val clientConfiguration = ClientConfiguration.builder()
+            .connectedTo("localhost:9200")
+            .withConnectTimeout(Duration.ofSeconds(10))
+            .withSocketTimeout(Duration.ofSeconds(10))
+            .build()
+
+        return RestClients.create(clientConfiguration).rest()
+    }
+}
